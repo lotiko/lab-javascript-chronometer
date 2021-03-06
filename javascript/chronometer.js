2d16bb6fd3b80312d146a7007c35e7f3973f7b2b
WalidBlila <walidbila8@gmail.com>
2021-03-06 16:19:33 +0000

    iteration 1 d

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -3,18 +3,18 @@ class Chronometer {
     this.currentTime = 0;
     this.intervalId = 0;
   }
-  startClick(callback) {
-    setInterval(function (){
-      this.currentTime += 1;
-    },1000);
 
-    
+  startClick(callback) {
+   this.intervalId = setInterval(() => this.currentTime += 1,1000);
+
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
+
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime)
   }
   twoDigitsNumber() {
     // ... your code goes here
